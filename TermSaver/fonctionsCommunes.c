4cd993e407c4317406937cc69b953f5b310c2d18
FlorianVouters <florian.vouters@viacesi.fr>
2016-12-12 09:59:23 +0100

    Ajout du fichier .c et .h des fonctions communes, oublie auparavent

---
+++ TermSaver/fonctionsCommunes.c
@@ -0,0 +1,1 @@
+#include "fonctionsCommunes.h"
---
+++ TermSaver/fonctionsCommunes.h
@@ -0,0 +1,13 @@
+#ifndef FONCTIONSCOMMUNES_H_INCLUDED
+#define FONCTIONSCOMMUNES_H_INCLUDED
+
+
+void afficherImg(int **tableau, int posX, int posY, int nbLignes);
+
+
+
+int ** loadImg(char* path);
+
+
+
+#endif // FONCTIONSCOMMUNES_H_INCLUDED
